e9a5ada06e3e53ef273463f4939b3a34a8f13574
Szabolcs Vasas <vasas@apache.org>
2020-12-20 17:16:58 +0100

    Skeleton project is generated.

---
+++ build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.4.21"
+}
+
+repositories {
+    jcenter()
+}
+
+dependencies {
+    val jupiterVersion = "5.6.2"
+    val kafkaVersion = "2.6.0"
+    val slf4jVersion = "1.7.30"
+    val assertjVersion = "3.18.1"
+    val testContainersVersion = "1.15.1"
+    val coroutinesVersion = "1.4.2"
+
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.apache.kafka:kafka-streams:$kafkaVersion")
+    implementation("org.slf4j:slf4j-simple:$slf4jVersion")
+    implementation(platform("org.testcontainers:testcontainers-bom:$testContainersVersion"))
+    implementation("org.testcontainers:kafka")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jupiterVersion")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:$jupiterVersion")
+    testImplementation("org.assertj:assertj-core:$assertjVersion")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+    testLogging.events("passed", "skipped")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
---
+++ src/main/kotlin/dev/vasas/kafkasuite/App.kt
@@ -0,0 +1,8 @@
+package dev.vasas.kafkasuite
+
+class App {
+    val greeting: String
+        get() {
+            return "Hello world."
+        }
+}
