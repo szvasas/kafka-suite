3146868c5b0026d231ef1abeebb49ccf20e324ed
Szabolcs Vasas <vasas@apache.org>
2020-12-27 18:41:24 +0100

    KafkaClusterAdmin with additional tests is implemented.

---
+++ src/main/kotlin/dev/vasas/kafkasuite/KafkaClusterAdmin.kt
@@ -0,0 +1,81 @@
+@file:JvmName("KafkaClusterAdmin")
+
+package dev.vasas.kafkasuite
+
+import dev.vasas.kafkasuite.cluster.KafkaCluster
+import org.apache.kafka.clients.admin.Admin
+import org.apache.kafka.clients.admin.AdminClient
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.admin.TopicDescription
+import org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG
+import org.apache.kafka.common.Node
+
+
+@JvmOverloads
+fun KafkaCluster.createTopic(topicName: String, numPartitions: Int = 1, replicationFactor: Short = 1) {
+    adminClient().use { admin ->
+        val newTopic = NewTopic(topicName, numPartitions, replicationFactor)
+        admin.createTopics(listOf(newTopic)).all().get()
+    }
+
+    verifyWithAllNodes { admin ->
+        admin.listTopicNames().contains(topicName)
+    }
+}
+
+fun KafkaCluster.deleteTopic(topicName: String) {
+    adminClient().use {
+        it.deleteTopics(listOf(topicName)).all().get()
+    }
+
+    verifyWithAllNodes { admin ->
+        admin.listTopicNames().contains(topicName)
+    }
+}
+
+fun KafkaCluster.listNodes(): Collection<Node> {
+    adminClient().use {
+        return it.describeCluster().nodes().get()
+    }
+}
+
+fun KafkaCluster.listTopics(): Collection<TopicDescription> {
+    adminClient().use { admin ->
+        val topicNames = admin.listTopicNames()
+        return admin.describeTopics(topicNames).all().get().values
+    }
+}
+
+private inline fun KafkaCluster.verifyWithAllNodes(block: (Admin) -> Boolean) {
+    bootstrapServers.split(",").forEach { server ->
+        waitUntil {
+            adminClient(server).use { admin ->
+                block(admin)
+            }
+        }
+    }
+}
+
+private inline fun waitUntil(block: () -> Boolean) {
+    val maxRetries = 20
+    var retry = 0
+    while (!block() && ++retry < maxRetries) {
+        Thread.sleep(100L)
+    }
+}
+
+private fun Admin.listTopicNames(): List<String> {
+    return listTopics().listings().get().map { it.name() }
+}
+
+private fun KafkaCluster.adminClient(): Admin {
+    return AdminClient.create(mapOf(
+            BOOTSTRAP_SERVERS_CONFIG to bootstrapServers
+    ))
+}
+
+private fun adminClient(bootstrapServers: String): Admin {
+    return AdminClient.create(mapOf(
+            BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+    ))
+}
--- src/test/kotlin/dev/vasas/kafkasuite/cluster/DockerKafkaClusterTest.kt
+++ src/test/kotlin/dev/vasas/kafkasuite/cluster/DockerKafkaClusterTest.kt
@@ -1,10 +1,17 @@
 package dev.vasas.kafkasuite.cluster
 
+import dev.vasas.kafkasuite.createTopic
+import dev.vasas.kafkasuite.deleteTopic
+import dev.vasas.kafkasuite.listNodes
+import dev.vasas.kafkasuite.listTopics
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.SoftAssertions.assertSoftly
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
-
 
 class DockerKafkaClusterTest {
 
@@ -14,13 +21,75 @@ class DockerKafkaClusterTest {
         assertThat(createDockerKafkaCluster(clusterSize).size).isEqualTo(clusterSize)
     }
 
-    @Test
-    fun `a 3 node Kafka cluster is initialized and started`() {
-        val kafkaCluster = createDockerKafkaCluster()
-        kafkaCluster.start()
+    @Nested
+    inner class `when a 3 node Kafka cluster is started` {
 
-        assertThat(kafkaCluster.isRunning).isTrue
+        lateinit var kafkaCluster: DockerKafkaCluster
 
-        kafkaCluster.stop()
+        @BeforeAll
+        fun beforeAll() {
+            kafkaCluster = createDockerKafkaCluster()
+            kafkaCluster.start()
+        }
+
+        @AfterAll
+        fun afterAll() {
+            kafkaCluster.stop()
+        }
+
+        @Test
+        fun `isRunning is true`() {
+            assertThat(kafkaCluster.isRunning).isTrue
+        }
+
+        @Test
+        fun `nodes with proper IDs are available`() {
+            val nodeIds = kafkaCluster.listNodes().map {
+                it.id()
+            }
+            assertThat(nodeIds).containsExactlyInAnyOrderElementsOf(1..kafkaCluster.size)
+        }
+
+        @Test
+        fun `there are no topics`() {
+            assertThat(kafkaCluster.listTopics()).isEmpty()
+        }
+
+        @Nested
+        inner class `and a new topic is created` {
+
+            val topicName = "myTestTopic"
+            val numPartitions = 2
+            val replicationFactor = 3
+
+            @BeforeAll
+            fun beforeAll() {
+                kafkaCluster.createTopic(topicName, numPartitions, replicationFactor.toShort())
+            }
+
+            @AfterAll
+            fun afterAll() {
+                kafkaCluster.deleteTopic(topicName)
+            }
+
+            @Test
+            fun `the topic is available in the cluster with correct parameters`() {
+                val topics = kafkaCluster.listTopics().filterNot { it.name().startsWith("__confluent") }
+
+                assertSoftly { softly ->
+                    softly.assertThat(topics.size).isEqualTo(1)
+
+                    val topic = topics.first()
+                    softly.assertThat(topic.name()).isEqualTo(topicName)
+                    softly.assertThat(topic.partitions().size).isEqualTo(numPartitions)
+
+                    topic.partitions().forEach {
+                        softly.assertThat(it.replicas().size).isEqualTo(replicationFactor)
+                    }
+
+                    softly.assertAll()
+                }
+            }
+        }
     }
 }
