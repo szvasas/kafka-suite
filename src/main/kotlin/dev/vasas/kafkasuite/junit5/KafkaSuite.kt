53666e2c216fcb58b87b3d168431072c63fe0fd9
Szabolcs Vasas <vasas@apache.org>
2020-12-31 19:55:40 +0100

    KafkaSuite interface is now properly annotated.

--- src/main/kotlin/dev/vasas/kafkasuite/junit5/KafkaSuite.kt
+++ src/main/kotlin/dev/vasas/kafkasuite/junit5/KafkaSuite.kt
@@ -3,7 +3,9 @@ package dev.vasas.kafkasuite.junit5
 import dev.vasas.kafkasuite.cluster.DockerKafkaCluster
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 interface KafkaSuite {
 
     val kafkaCluster: DockerKafkaCluster
